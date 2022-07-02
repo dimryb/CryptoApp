a04b96e021b7e25a2b89dfba31042939c3994649
dmitry <dimryb@bk.ru>
2022-07-02 21:53:01 +0700

    Доработал чтобы собиралось

--- app/src/main/java/com/example/cryptoapp/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/ViewModelFactory.kt
@@ -6,11 +6,10 @@ import javax.inject.Inject
 import javax.inject.Provider
 
 class ViewModelFactory @Inject constructor(
-    private val viewModelProviders:  @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
-): ViewModelProvider.Factory {
+    private val viewModelProviders: @JvmSuppressWildcards Map<Class<out ViewModel>, Provider<ViewModel>>
+) : ViewModelProvider.Factory {
 
-    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         return viewModelProviders[modelClass]?.get() as T
     }
-
 }
