cbae5886281e1303be61660f90d28bddd20e5b9c
dmitry <dimryb@bk.ru>
2022-07-03 22:09:26 +0700

    Инъекцию зависимостей в фабрику воркеров

--- app/src/main/java/com/example/cryptoapp/presentation/CoinApp.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/CoinApp.kt
@@ -8,21 +8,26 @@ import com.example.cryptoapp.data.network.ApiFactory
 import com.example.cryptoapp.data.network.ApiService
 import com.example.cryptoapp.data.workers.RefreshDataWorkerFactory
 import com.example.cryptoapp.di.DaggerApplicationComponent
+import javax.inject.Inject
 
 class CoinApp: Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: RefreshDataWorkerFactory
 
     val component by lazy {
         DaggerApplicationComponent.factory().create(this)
     }
 
+    override fun onCreate() {
+        component.inject(this)
+        super.onCreate()
+    }
+
     override fun getWorkManagerConfiguration(): Configuration {
         return Configuration.Builder()
             .setWorkerFactory(
-                RefreshDataWorkerFactory(
-                    AppDatabase.getInstance(this).coinPriceInfoDao(),
-                    ApiFactory.apiService,
-                    CoinMapper()
-                )
+                workerFactory
             )
             .build()
     }
